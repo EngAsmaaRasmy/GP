3dbfce4c40f0eacf1545be1db0c27a6a868c5734
hend shabaan <hendshabaan123z@gmail.com>
2022-07-11 23:23:14 +0300

    first commit

--- database/seeders/SchadulesSeeder.php
+++ database/seeders/SchadulesSeeder.php
@@ -16,12 +16,8 @@ class SchadulesSeeder extends Seeder
     public function run()
     {
         $schedules = [
-            [
-                'doctor_id' => 1,
-                'week_day' => 'Saturday',
-                'time' => '5-7',
-                'created_at' => Carbon::now(),
-            ],
+          
+
         ];
 
         Schadule::insert($schedules);
